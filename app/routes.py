66cf728bbba963fcff383db176bba99b9b3db31a
Adam <adamlives@hotmail.com>
2020-06-28 19:00:05 +0100

    add model support in template

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,21 @@
 from flask import render_template
 
 from app import app
+from app.models import Plant, Watering
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Seed'}
+    user = {'username': 'Adam'}
+    plants = Plant.query.all()
+    '''for plant in plants:
+        pw = Plant.query
+            .join(Watering)
+            .add_columns(Plant.name, Plant.location, Watering.timestamp)
+            .filter(Plant.name==plant.name)
+            .order_by(Watering.timestamp.desc())
+            .all()
+
     plants = [
         {
             'name': 'Spike',
@@ -19,5 +29,5 @@ def index():
             'last_watered_time': '08:30',
             'last_watered_date': '27Jun2020'
         }
-    ]
+    ]'''
     return render_template('index.html', title='The Constant Gardener', user=user, plants=plants)

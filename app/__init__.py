32e263e5b2590d96893e4da4044aba7e8254da51
Adam <adamlives@hotmail.com>
2020-06-29 23:28:54 +0100

    add datetime read from db and last watered field

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_moment import Moment
 
 from flask import Flask
 from config import Config
@@ -8,5 +9,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+moment = Moment(app)
 
 from app import routes, models

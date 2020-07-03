9ae9128f949b76dc8f774933c83c26168b2e2323
Adam <adamlives@hotmail.com>
2020-07-03 14:42:32 +0100

    add support for .env file

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-dev-key'
@@ -11,7 +14,7 @@ class Config(object):
     TOP_LEVEL_DIR = os.path.abspath(os.curdir)
 
     UPLOADS_DEFAULT_DEST = TOP_LEVEL_DIR + '/app/static/img/'
-    UPLOADS_DEFAULT_URL = 'http://localhost:5000/static/img/'
+    UPLOADS_DEFAULT_URL = os.environ.get('UPLOADS_DEFAULT_URL') or 'http://localhost:5000/static/img/'
 
     UPLOADED_IMAGES_DEST = TOP_LEVEL_DIR + '/app/static/img/'
-    UPLOADED_IMAGES_URL = 'http://localhost:5000/static/img/'
+    UPLOADED_IMAGES_URL = os.environ.get('UPLOADED_IMAGES_URL') or 'http://localhost:5000/static/img/'
